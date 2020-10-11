ffdfc1b2b27d4f221676524374c74d386065c006
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2020-10-10 21:22:37 -0400

    Add files via upload
    PJJC auto switch 3 team to defense

---
+++ PjjcAuto.java
@@ -0,0 +1,361 @@
+package week3arrays;
+
+public class PjjcAuto {
+    public static void main(String[] args) {
+        Dim Homex, Homey
+        Randomize
+                Homex = Int((790 - 690 + 1) * Rnd() + 690)
+        Homey = Int((780 - 757 + 1) * Rnd() + 757)
+
+        Tap Homex, Homey
+        Delay round (4678 * rnd() + 1385)
+
+        Dim Pjjcx, Pjjcy
+        Randomize
+                Pjjcx = Int((1380 - 1080 + 1) * Rnd() + 1080)
+        Pjjcy = Int((694 - 550 + 1) * Rnd() + 550)
+
+        Tap Pjjcx, Pjjcy
+        Delay round (4678 * rnd() + 1385)
+
+        Dim Defendx, Defendy
+        Randomize
+                Defendx = Int((480 - 300 + 1) * Rnd() + 300)
+        Defendy = Int((670 - 620 + 1) * Rnd() + 620)
+
+        Tap Defendx, Defendy
+        Delay round (4678 * rnd() + 885)
+
+        Dim Myteam1x, Myteam1y
+        Randomize
+                Myteam1x = Int((190 - 90 + 1) * Rnd() + 90)
+        Myteam1y = Int((136 - 123 + 1) * Rnd() + 123)
+
+        Tap Myteam1x, Myteam1y
+        Delay round (4678 * rnd() + 885)
+
+        Dim Avatarx1, Avatary1
+        Randomize
+                Avatarx1 = Int((858 - 750 + 1) * Rnd() + 750)
+        Avatary1 = Int((730 - 622 + 1) * Rnd() + 622)
+
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+
+        Dim Myteam2x, Myteam2y
+        Randomize
+                Myteam2x = Int((370 - 266 + 1) * Rnd() + 266)
+        Myteam2y = Int((136 - 123 + 1) * Rnd() + 123)
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+        Dim Avatarx2, Avatary2
+        Randomize
+                Avatarx2 = Int((858 - 750 + 1) * Rnd() + 750)
+        Avatary2 = Int((730 - 622 + 1) * Rnd() + 622)
+
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+
+        Dim Myteam3x, Myteam3y
+        Randomize
+                Myteam3x = Int((550 - 433 + 1) * Rnd() + 433)
+        Myteam3y = Int((136 - 123 + 1) * Rnd() + 123)
+
+        Tap Myteam3x, Myteam3y
+        Delay round (4678 * rnd() + 885)
+
+        Dim Avatarx, Avatary
+        Randomize
+                Avatarx = Int((858 - 750 + 1) * Rnd() + 750)
+        Avatary = Int((730 - 622 + 1) * Rnd() + 622)
+
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+
+        Tap Myteam1x, Myteam1y
+        Delay round (2678 * rnd() + 485)
+
+        Dim Teamx, Teamy
+        Randomize
+                Teamx = Int((1350 - 1250 + 1) * Rnd() + 1250)
+        Teamy = Int((240 - 229 + 1) * Rnd() + 229)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Dim Sec1x, Sec1y
+        Randomize
+                Sec1x = Int((270 - 90 + 1) * Rnd() + 90)
+        Sec1y = Int((133 - 123 + 1) * Rnd() + 123)
+
+        Tap Sec1x, Sec1y
+        Delay round (2345 * rnd() + 433)
+
+        Dim Copy1x, Copy1y
+        Randomize
+                Copy1x = Int((1277 - 1097 + 1) * Rnd() + 1097)
+        Copy1y = Int((275 - 258 + 1) * Rnd() + 258)
+
+        Tap Copy1x, Copy1y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec1x, Sec1y
+        Delay round (2345 * rnd() + 433)
+
+        Dim Copy2x, Copy2y
+        Randomize
+                Copy2x = Int((1277 - 1097 + 1) * Rnd() + 1097)
+        Copy2y = Int((440 - 430 + 1) * Rnd() + 430)
+
+        Tap Copy2x, Copy2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam3x, Myteam3y
+        Delay round (3678 * rnd() + 333)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec1x, Sec1y
+        Delay round (2345 * rnd() + 433)
+
+        Dim Copy3x, Copy3y
+        Randomize
+                Copy3x = Int((1277 - 1097 + 1) * Rnd() + 1097)
+        Copy3y = Int((620 - 610 + 1) * Rnd() + 610)
+
+        Tap Copy3x, Copy3y
+        Delay round (2345 * rnd() + 433)
+
+        Dim Finishx, Finishy
+        Randomize
+                Finishx = Int((1291 - 1122 + 1) * Rnd() + 1222)
+        Finishy = Int((670 - 660 + 1) * Rnd() + 660)
+
+        Tap Finishx, Finishy
+        Delay round (4567 * rnd() + 888)
+
+
+        Tap Defendx, Defendy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Defendx, Defendy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Myteam1x, Myteam1y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+
+        Tap Myteam3x, Myteam3y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+
+        Tap Myteam1x, Myteam1y
+        Delay round (2678 * rnd() + 485)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Dim Sec2x, Sec2y
+        Randomize
+                Sec2x = Int((485 - 310 + 1) * Rnd() + 310)
+        Sec2y = Int((133 - 123 + 1) * Rnd() + 123)
+
+        Tap Sec2x, Sec2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy1x, Copy1y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec2x, Sec2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy2x, Copy2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam3x, Myteam3y
+        Delay round (3678 * rnd() + 333)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec2x, Sec2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy3x, Copy3y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Finishx, Finishy
+        Delay round (4567 * rnd() + 888)
+
+
+        Tap Defendx, Defendy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Defendx, Defendy
+        Delay round (4678 * rnd() + 885)
+
+
+        Tap Myteam1x, Myteam1y
+        Delay round (4678 * rnd() + 885)
+
+
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx1, Avatary1
+        Delay round (1678 * rnd() + 100)
+
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx2, Avatary2
+        Delay round (1678 * rnd() + 100)
+
+
+        Tap Myteam3x, Myteam3y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+
+        Tap Myteam1x, Myteam1y
+        Delay round (2678 * rnd() + 485)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Dim Sec3x, Sec3y
+        Randomize
+                Sec2x = Int((699 - 522 + 1) * Rnd() + 522)
+        Sec2y = Int((133 - 123 + 1) * Rnd() + 123)
+
+        Tap Sec3x, Sec3y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy1x, Copy1y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam2x, Myteam2y
+        Delay round (4678 * rnd() + 885)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec3x, Sec3y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy2x, Copy2y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Myteam3x, Myteam3y
+        Delay round (3678 * rnd() + 333)
+
+        Tap Teamx, Teamy
+        Delay round (4678 * rnd() + 885)
+
+        Tap Sec3x, Sec3y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Copy3x, Copy3y
+        Delay round (2345 * rnd() + 433)
+
+        Tap Finishx, Finishy
+        Delay round (4567 * rnd() + 888)
+    }
+}
