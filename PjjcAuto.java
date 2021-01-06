467bb4ef94a23bf08cc0642c6655c10ee76ae4d7
Stephen Zheng <65367318+i-le@users.noreply.github.com>
2021-01-06 18:26:57 -0500

    Update PjjcAuto.java

--- PjjcAuto.java
+++ PjjcAuto.java
@@ -357,5 +357,16 @@ public class PjjcAuto {
 
         Tap Finishx, Finishy
         Delay round (4567 * rnd() + 888)
+            
+            Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
+        Tap Avatarx, Avatary
+        Delay round (1678 * rnd() + 100)
     }
 }
